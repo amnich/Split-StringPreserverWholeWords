165b2a62fe4acbc5549450c783a1d1c00c1e171f
Adam Mnich <adammnich@gmail.com>
2019-11-22 14:06:46 +0100

    Update Split-StringPreserverWholeWords.ps1

--- Split-StringPreserverWholeWords.ps1
+++ Split-StringPreserverWholeWords.ps1
@@ -10,7 +10,8 @@ function Split-StringPreserverWholeWords {
     if (!$OldString) {
         $OldString = $string
     }
-    if ($string.Length -gt $splitLength) {
+    $global:myvar = $null
+    $return = if ($string.Length -gt $splitLength) {
         $lastIndex = ($string[0..$splitLength] -join '').LastIndexOf($Divider)
         if ($lastIndex -eq $null -or $lastIndex -le 0) {
             if ($FindSmallestDivider -and $i -ne $SplitLength) {
@@ -18,12 +19,14 @@ function Split-StringPreserverWholeWords {
                 while ($SplitLength -lt $OldString.length) {                                        
                     try {
                         Split-StringPreserverWholeWords -string $OldString -SplitLength $i -FindSmallestDivider:$false -Divider $Divider | out-null 
-                        Write-Warning "Split failed. Smallest divider is $i"
-                        return                        
+                        Write-Warning "Split failed. Smallest divider is $i"                                                
+                        [int]$global:myvar = $i
+                        return $i
                     }
                     catch {
-                        $i++
+                        $i++                        
                     }
+                    
                 }
             }
             else {
@@ -35,10 +38,18 @@ function Split-StringPreserverWholeWords {
         $newString.trim()
         if ($string.Length - $newString.Length -gt $splitLength) {
             Split-StringPreserverWholeWords -string ($string[$($lastIndex+1)..$($string.Length)] -join '').trim() -splitLength $splitLength -OldString $OldString -FindSmallestDivider:$FindSmallestDivider -Divider $Divider
-        }
-    
+        } 
+        else{
+            $string[$($lastIndex+1)..$($string.Length)] -join ''
+        }    
     }
     else {
         $string
+    }
+    if ($global:myvar){
+        return [int]$global:myvar        
+    }
+    else{
+        $return
     }
 }
